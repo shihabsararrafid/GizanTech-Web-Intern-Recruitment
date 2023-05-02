1e979651f7e55a616b0c8097091a1287ee410506
shihabsararrafid <shrafid.532@gmail.com>
2023-05-02 11:23:10 +0600

    data shown in view engine

--- task1/controllers/exercise.controller.js
+++ task1/controllers/exercise.controller.js
@@ -3,29 +3,37 @@ const { loadUser } = require("../Middlewares/loadUser");
 const { loadbodyParts } = require("../Middlewares/loadbodyParts");
 
 module.exports.getBodyParts = async (req, res, next) => {
-  try {
-    const val = loadbodyParts();
-    // res.status(200).json({
-    //   status: "Success",
-    //   message: "Data Loaded Successfully",
-    //   bodyparts: val,
-    // });
-    res.render("pages/bodypart");
-  } catch (error) {
-    console.log("hello error");
-  }
-  // loadUser()
-  //   .then((value) => {
-  //     const parsedData = JSON.parse(value);
+  // try {
+  //   const val = loadbodyParts();
+  //   // res.status(200).json({
+  //   //   status: "Success",
+  //   //   message: "Data Loaded Successfully",
+  //   //   bodyparts: val,
+  //   // });
+  //   res.render("pages/bodypart");
+  // } catch (error) {
+  //   console.log("hello error");
+  // }
+  loadUser()
+    .then((value) => {
+      const parsedData = JSON.parse(value);
 
-  //     //  const bodyparts = parsedData.map((exercies) => exercies.bodyPart);
-  //     res.status(200).json({
-  //       status: "Success",
-  //       message: "Data Loaded Successfully",
-  //       bodyparts: bodyParts,
-  //     });
-  //   })
-  //   .catch((error) => {
-  //     console.log(error.message);
-  //   });
+      //  const bodyparts = parsedData.map((exercies) => exercies.bodyPart);
+      // res.status(200).json({
+      //   status: "Success",
+      //   message: "Data Loaded Successfully",
+      //   bodyparts: bodyParts,
+      // });
+      res
+        .status(200)
+        .render("pages/bodypart", { data: parsedData, title: "All Bodyparts" });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        status: "Fail",
+        message: "Failed to load data from db",
+        error: error.message,
+      });
+      console.log(error.message);
+    });
 };
