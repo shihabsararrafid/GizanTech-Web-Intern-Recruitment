efb3cfefe233ffbcd74ef9618167c4803616fd7c
shihabsararrafid <shrafid.532@gmail.com>
2023-05-02 13:13:23 +0600

    git by id done

--- task1/controllers/exercise.controller.js
+++ task1/controllers/exercise.controller.js
@@ -65,3 +65,36 @@ module.exports.getExerciseByBodyPart = async (req, res, next) => {
       console.log(error.message);
     });
 };
+
+module.exports.getExerciseById = async (req, res, next) => {
+  loadData()
+    .then((val) => {
+      const { id } = req.params;
+      const parsedData = JSON.parse(val);
+      let exercises = [];
+      // res.send(bodyPart);
+      setTimeout(() => {
+        parsedData.forEach((element) => {
+          if (element.id === id) {
+            exercises.push(element);
+          }
+        });
+      }, 1000);
+      setTimeout(() => {
+        //  rendering ejs file as a response from view folder
+        res.status(200).render("pages/exercises", {
+          data: exercises,
+          title: ` Exercise for id ${id}`,
+        });
+        // res.send(exercises);
+      }, 1500);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        status: "Fail",
+        message: "Failed to load data from db",
+        error: error.message,
+      });
+      console.log(error.message);
+    });
+};
--- task1/routes/v1/exercise.route.js
+++ task1/routes/v1/exercise.route.js
@@ -27,7 +27,7 @@ exerciseRoute
   .route("/getExerciseByBodyPart/:bodyPart")
   /**
    * @api {get}
-   * @apiDescription Get all exercise from bodypart
+   * @apiDescription Get all exercise for a certain bodyPart
    * @apiPermission admin
    *
    * @apiHeader {String} Authorization   User's access token
@@ -40,5 +40,24 @@ exerciseRoute
    * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
    * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
    */ .get(exerciseController.getExerciseByBodyPart);
+
+exerciseRoute
+  .route("/getById/:id")
+  /**
+   * @api {get}
+   * @apiDescription Get all the users from the database
+   * @apiPermission admin
+   *
+   * @apiHeader {String} Authorization   User's access token
+   *
+   * @apiParam  {Number{1-}}         [page=1]     List page
+   * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+   *
+   * @apiSuccess {Object[]} all the users.
+   *
+   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+   * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+   */
+  .get(exerciseController.getExerciseById);
 
 module.exports = exerciseRoute;
